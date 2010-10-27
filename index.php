ed3d8991db6e76fc7c76a70ae26dfa5d1a072912
giushansen <fourretg@gmail.com>
2010-10-27 16:59:53 +1100

    undo test git

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 | By default CI runs with error reporting set to ALL.  For security
 | reasons you are encouraged to change this when your site goes live.
 | For more info visit:  http://www.php.net/error_reporting
-|boudin
+|
 */
 	error_reporting(E_ALL);
 
