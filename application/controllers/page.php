73a7a9701e16eed16e31ce8a92058e0b5d7c4493
giushansen <fourretg@gmail.com>
2011-02-24 15:21:52 +1100

    Include the Box management when displaying the pages

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -5,6 +5,7 @@ class Page extends Public_Controller {
     function __construct()
     {
         parent::__construct();
+        $this->load->helper('file');
     }
 
     	/**
@@ -42,8 +43,35 @@ class Page extends Public_Controller {
                 show_404($url_segments[1]);
             }
         }
+
+        //---------------------------------------------------------------------
+        // LOAD THE BLOCKS
+        //---------------------------------------------------------------------
         
-        $this->aData['Body'] = $this->oPage->body;
+        // This page has block(s) ?
+        $this->aAllBlocks = $this->blocks_m->getBlocksByPageID($this->oPage->id);
+        // Check if there are Blocks in this page
+        if ($this->aAllBlocks == true){
+            // Browse each column of this page
+            foreach($this->aAllBlocks as $col=>$aBlock){
+                // Browse each block of this column and store the block in a ordered view array
+                foreach ($aBlock as $key=>$oBlock){
+
+                    $this->aBlocks[$col][$oBlock->position[1]] = $this->_loadBlockView($oBlock->block_name, $oBlock->block_id);
+
+                }
+                ksort($this->aBlocks[$col]);
+            }
+
+            $this->aData['aBlocks'] = $this->aBlocks;
+        }
+
+        //---------------------------------------------------------------------
+        // LOAD THE PAGE
+        //---------------------------------------------------------------------
+
+        //$this->aData['Body'] = $this->oPage->body;
+
         $this->aData['sBaseURL']  = $this->sBaseUrl;
 
         $this->template->set_title($this->oPage->title)
@@ -51,7 +79,33 @@ class Page extends Public_Controller {
                 ->set_metadata('description', $this->oPage->meta_description)
                 ->set_metadata('keywords', $this->oPage->meta_keywords)
                 ->set_menu($aNav)
-                ->build('pages/content_two_view', $this->aData);
+                ->build('pages/'.$this->oPage->view.'_view', $this->aData);
+    }
+
+    // Load the controller class of the block that matches the block name
+    public function _loadBlockView($block_name = '', $block_id = 0)
+    {      
+        switch($block_name){
+
+            case 'content':
+                require_once './'.APPPATH.'libraries/blocks/content_blocks'.EXT;
+                $oContentBlock = new content_blocks();
+                return $oContentBlock->getBlockView($block_id);
+
+            case 'tbox':
+                require_once './'.APPPATH.'libraries/blocks/tbox_blocks'.EXT;
+                $oTboxBlock = new tbox_blocks();
+                return $oTboxBlock->getBlockView($block_id);
+
+            case 'contact':
+                require_once './'.APPPATH.'libraries/blocks/contact_blocks'.EXT;
+                $oContactBlock = new contact_blocks();
+                return $oContactBlock->getBlockView($block_id);
+
+            default :
+                return false;
+                break;
+        }
     }
 
 }
